ccb54ccb2f9c6284ffe248f4af28e7093f50854d
YIDA <guel18gomez@gmail.com>
2020-04-21 20:09:29 -0500

    Inicio de caja

---
+++ models/caja.php
@@ -0,0 +1,14 @@
+class Cash{
+	public $id_persona;
+    public $nombrePers;
+    public $apellido;
+    public $fecha_nac;
+    public $direccion;
+    public $telefono;
+    public $usuario;
+    public $contrasena;
+    public $foto;
+    public $comprobante;
+    public $num_empleado;
+    public $id_rol;
+}
---
+++ views/caja/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Caja</title>
+</head>
+
+<body>
+	<?php require 'views/header.php'; ?>
+	<?php require 'views/menu.php'; ?>
+
+	<div class="container-fluid">
+		<div id="respuesta" class="center"></div>
+		<div class="container justify-content-center" id="caja">
+			jorjor
+		</div>
+	</div>
+
+<?php require 'views/footer.php'; ?>
+	<script src="<?php echo constant('URL'); ?>public/js/main.js"></script>
+</body>
+</html>
