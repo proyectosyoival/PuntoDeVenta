1e5fdcc5479319ef5ad34241abb88345f774747c
Yovani <guel18gomez@gmail.com>
2020-05-20 20:57:30 -0500

    config local

--- config/config.php
+++ config/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 define('URL', 'http://localhost/PuntoDeVenta/');
@@ -11,17 +10,3 @@ define('PASSWORD', "");
 define('CHARSET', 'utf8');
 
 ?>
-=======
-<?php
-
-define('URL', 'http://localhost/PuntoDeVenta/');
-
-define('HOST', '192.168.0.190:3306');
-// define('HOST', 'localhost');
-define('DB', 'PuntoDeVenta');
-define('USER', 'root');
-define('PASSWORD', "");
-define('CHARSET', 'utf8');
-
-?>
->>>>>>> f5c768beee4119759eed10e6e9924b14549dfe27
