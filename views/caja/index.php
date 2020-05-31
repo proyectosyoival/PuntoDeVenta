516f3ed13b714ca8dbc4a3ebbb3ece44bf6673cd
Yovani <guel18gomez@gmail.com>
2020-05-31 09:32:29 -0500

    diseño pestañas caja

--- views/caja/index.php
+++ views/caja/index.php
@@ -13,10 +13,10 @@
 		<div id="caja">
 
   <div class="nav nav-tabs" id="nav-tab" role="tablist">
-    <a class="nav-item nav-link active" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Apertura de Turno</a>
-    <a class="nav-item nav-link" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Cobro</a>
-    <a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Apartado</a>
-		<a class="nav-item nav-link" id="nav-contact-tab" data-toggle="tab" href="#nav-apartado" role="tab" aria-controls="nav-contact" aria-selected="false">Movimientos</a>
+    <a class="nav-item nav-link active pestanas font-weight-bold" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Apertura de Turno</a>
+    <a class="nav-item nav-link pestanas font-weight-bold" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false">Cobro</a>
+    <a class="nav-item nav-link pestanas font-weight-bold" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false">Apartado</a>
+		<a class="nav-item nav-link pestanas font-weight-bold" id="nav-contact-tab" data-toggle="tab" href="#nav-apartado" role="tab" aria-controls="nav-contact" aria-selected="false">Movimientos</a>
   </div>
 
 <div class="tab-content" id="nav-tabContent">
